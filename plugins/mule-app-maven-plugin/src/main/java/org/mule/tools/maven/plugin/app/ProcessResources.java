572903e21d2bb7c0705993813d633a29b4139202
William Pinho <william.comp.ita@gmail.com>
2018-07-20 10:02:07 -0300

    Refactor and add tests

--- plugins/mule-app-maven-plugin/src/main/java/org/mule/tools/maven/plugin/app/ProcessResources.java
+++ plugins/mule-app-maven-plugin/src/main/java/org/mule/tools/maven/plugin/app/ProcessResources.java
@@ -13,31 +13,25 @@ package org.mule.tools.maven.plugin.app;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.mule.tools.maven.plugin.MuleExportProperties;
+import org.mule.tools.maven.plugin.ZipArchiver;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Properties;
-import java.util.Set;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+
+import static org.mule.tools.maven.plugin.MuleExportProperties.MULE_EXPORT_PROPERTIES_FILE;
 
 /**
+ * This mojo generates the META-INF folder with the necessary files to make the zip
+ * importable in Studio.
+ *
  * @phase process-resources
  * @goal process-resources
  */
 public class ProcessResources extends AbstractMuleMojo {
 
   public static final String META_INF_FOLDER = "META-INF";
-  public static final String MULE_EXPORT_PROPERTIES_FILE = "mule_export.properties";
-  public static final String MULE_EXPORT_VERSION_KEY = "mule_export_version";
-  public static final String MULE_EXPORT_VERSION_VALUE = "2.0";
-  public static final String MULE_EXPORTED_PROJECTS_KEY = "mule_exported_projects";
   public static final String[] BLACK_LIST = new String[]{".DS_STORE", "target"};
-  private static final Set<String> BLACK_LIST_SET = new HashSet<>(Arrays.asList(BLACK_LIST));
 
   /**
    * Whether this project should attach mule sources to make it importable in Studio.
@@ -49,70 +43,66 @@ public class ProcessResources extends AbstractMuleMojo {
 
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-    if(attachMuleSources) {
+    if (attachMuleSources) {
+      File metaInfFolder = createMetaInfFolderInTarget();
+
       File projectBaseFolder = project.getBasedir();
-      File targetFolder = new File(project.getBuild().getDirectory());
-      String projectName = projectBaseFolder.getName();
-      File metaInfFolder = new File(targetFolder, META_INF_FOLDER);
-
-      metaInfFolder.mkdir();
-
-      generateMuleExportProperties(metaInfFolder, projectName);
       generateExportedZipFile(metaInfFolder, projectBaseFolder);
+
+      String projectName = projectBaseFolder.getName();
+      generateMuleExportProperties(metaInfFolder, projectName);
     } else {
       getLog().info("attachMuleSources default value is false, skipping process-resources goal...");
     }
   }
 
-  private void generateExportedZipFile(File exportableFolder, File projectBaseFolder) {
-    FileOutputStream fos = null;
-    try {
-      fos = new FileOutputStream(exportableFolder.getAbsolutePath() + File.separator + projectBaseFolder.getName() + ".zip");
-      ZipOutputStream zipOut = new ZipOutputStream(fos);
+  /**
+   * Creates the META-INF folder in the target directory.
+   *
+   * @return the file representing META-INF
+   */
+  private File createMetaInfFolderInTarget() {
+    File targetFolder = new File(project.getBuild().getDirectory());
+    File metaInfFolder = new File(targetFolder, META_INF_FOLDER);
+    metaInfFolder.mkdir();
+    return metaInfFolder;
+  }
 
-      zipFile(projectBaseFolder, projectBaseFolder.getName(), zipOut);
-      zipOut.close();
-      fos.close();
+  /**
+   * Generates a zip file containing all the project content. The file is named as the projectBaseFolderName + .zip
+   *
+   * @param metaInfFolder
+   * @param projectBaseFolder
+   * @throws MojoExecutionException
+   */
+  protected void generateExportedZipFile(File metaInfFolder, File projectBaseFolder) throws MojoExecutionException {
+    ZipArchiver archiver = getZipArchiver();
+    String output = metaInfFolder.getAbsolutePath() + File.separator + projectBaseFolder.getName() + ".zip";
+    try {
+      archiver.toZip(projectBaseFolder, output);
     } catch (IOException e) {
-      e.printStackTrace();
+      throw new MojoExecutionException("Could not create exportable zip file", e);
     }
   }
 
-  private void generateMuleExportProperties(File exportableFolder, String projectName) throws MojoExecutionException {
-    try (FileOutputStream output = new FileOutputStream(new File(exportableFolder, MULE_EXPORT_PROPERTIES_FILE))) {
-      Properties muleExportProperties = new Properties();
-      muleExportProperties.setProperty(MULE_EXPORT_VERSION_KEY, MULE_EXPORT_VERSION_VALUE);
-      muleExportProperties.setProperty(MULE_EXPORTED_PROJECTS_KEY, projectName);
-      muleExportProperties.store(output, null);
+  /**
+   * Generates the mule_export.properties file in META-INF
+   *
+   * @param metaInfFolder
+   * @param projectName
+   * @throws MojoExecutionException
+   */
+  protected void generateMuleExportProperties(File metaInfFolder, String projectName) throws MojoExecutionException {
+    MuleExportProperties exportProperties = new MuleExportProperties(projectName);
+    try {
+      exportProperties.store(metaInfFolder);
     } catch (IOException e) {
       throw new MojoExecutionException("Could not create " + MULE_EXPORT_PROPERTIES_FILE, e);
     }
   }
 
-  private static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
-    if (!isInBlackList(fileToZip)) {
-      if (fileToZip.isDirectory()) {
-        File[] children = fileToZip.listFiles();
-        ZipEntry zipEntry = new ZipEntry(fileName + "/");
-        zipOut.putNextEntry(zipEntry);
-        for (File childFile : children) {
-          zipFile(childFile, fileName + "/" + childFile.getName(), zipOut);
-        }
-      } else {
-        FileInputStream fis = new FileInputStream(fileToZip);
-        ZipEntry zipEntry = new ZipEntry(fileName);
-        zipOut.putNextEntry(zipEntry);
-        byte[] bytes = new byte[1024];
-        int length;
-        while ((length = fis.read(bytes)) >= 0) {
-          zipOut.write(bytes, 0, length);
-        }
-        fis.close();
-      }
-    }
-  }
 
-  private static boolean isInBlackList(File fileToZip) {
-    return BLACK_LIST_SET.contains(fileToZip.getName());
+  public ZipArchiver getZipArchiver() {
+    return new ZipArchiver(BLACK_LIST);
   }
 }
